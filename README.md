876fee7928b12d8189417ebbf3c0b4b5b1f2546d
weaming <garden.yuen@gmail.com>
2021-01-22 09:14:20 +0800

    more redable readme

--- README.md
+++ README.md
@@ -2,8 +2,27 @@
 
 list git references(branches, tags) on local and remote repositories.
 
+## Install
+
+    pip3 install -U git-refs
+
+## Example
+
+```bash
+$ git-refs
+local branch master 5a4344acd93627b7e20d5c86aa18a587c90a17d9 -> origin/master
+remote reference master 5a4344acd93627b7e20d5c86aa18a587c90a17d9 origin
 ```
-> git-refs
-local branch master 13f0980d8dc5a3e23b60d7ff80896c2193f4f255 -> origin/master
-remote reference master 13f0980d8dc5a3e23b60d7ff80896c2193f4f255
+
+### CSV output format
+
+```bash
+$ CSV=1 git-refs
+where,type,name,sha,remote
+local,branch,master,5a4344acd93627b7e20d5c86aa18a587c90a17d9,origin/master
+remote,reference,master,5a4344acd93627b7e20d5c86aa18a587c90a17d9,origin
 ```
+
+You can use [`printable`](https://github.com/weaming/printable) to view csv in terminal:
+
+    CSV=1 git-refs | printable -t csv -f /dev/stdin
--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
+# FYI:
+# https://packaging.python.org/tutorials/packaging-projects/
+# https://realpython.com/pypi-publish-python-package/
+
 from setuptools import setup
 gh_repo = 'https://github.com/weaming/git-refs'
 
 setup(
     name='git-refs',  # Required
-    version='0.2',  # Required
+    version='1.1',  # Required
     description='list git references(branches, tags) on local and remote repositories',  # Required
+    long_description=open('README.md').read().strip(),
+    long_description_content_type="text/markdown",
     url=gh_repo,  # Optional
     author='weaming',  # Optional
     author_email='garden.yuen@gmail.com',  # Optional
+    install_requires=['gitpython'],
     py_modules=['git_refs'],
-    keywords='git,cli,develop',  # Optional
     entry_points={  # Optional
         'console_scripts': [
             'git-refs=git_refs:main',
         ],
     },
-    install_requires=['gitpython'],
+    keywords='git,cli,develop',  # Optional
     project_urls={  # Optional
         'Bug Reports': gh_repo,
         'Source': gh_repo,
